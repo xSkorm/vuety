6313b650342c62892319925ac88cd51087d860e4
xSkorm <simon.tranter@prospectsoft.com>
2016-12-16 12:27:16 +0000

    Export custom decorator functions

--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ export { Render } from "./src/render";
 export { Prop } from "./src/prop";
 export { Data } from "./src/data";
 export { Watch } from "./src/watch";
+export { RegisterDecorator, TypedMemberDecorator } from "./src/core";
